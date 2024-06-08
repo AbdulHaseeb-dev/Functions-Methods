ba958725c2f0e41bdbf405510383c78d9b0591ae
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-08 11:30:46 +0500

    Printed binomial coff using functions

--- Factorial.java
+++ Factorial.java
@@ -9,12 +9,24 @@ public class Factorial {
         }
         return f;
     }
+
+    public static int binCoeff (int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binCoeff = fact_n/ (fact_r *fact_nmr);
+        return binCoeff;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter n");
         int n = sc.nextInt();
-        int f = factorial(n);
-        System.out.println(f);
+        System.out.println("enter r");
+        int r = sc.nextInt();
+        // int q = factorial(r);
+        // int f = factorial(n);
+        System.out.println(binCoeff(n, r));
         sc.close();
     }
 }
